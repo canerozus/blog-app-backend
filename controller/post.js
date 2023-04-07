246d267699a75151775c17a461dc4b327ac6b514
deepforce08 <83680245+canerozus@users.noreply.github.com>
2023-04-07 17:19:47 +0300

    post create

--- controller/post.js
+++ controller/post.js
@@ -2,28 +2,37 @@ const multer = require('multer');
 const fs = require('fs');
 const Post = require('../models/post');
 
-const uploadMiddleware = multer({ dest: './uploads' }).single('file');
-
 const post = async (req, res) => {
-    const { title, summary, content } = req.body
-    console.log(req.body)
-    uploadMiddleware(req, res, (err) => {
+    const uploadMiddleware = multer({ dest: './uploads' }).single('file');
+    uploadMiddleware(req, res, async (err) => {
         if (err) {
-            res.status(500).send(err);
-        } else {
-            const { originalname, path } = req.file
+            console.error(err);
+            return res.status(500).json({ message: "Failed to upload" });
+        }
+        const { title, summary, content } = req.body;
+        console.log({title});
+        let fileUrl;
+        if (req.file) {
+            const { originalname, path } = req.file;
             const parts = originalname.split('.');
             const ext = parts[parts.length - 1];
             const filename = path + '.' + ext;
-            fs.renameSync(path, filename)
+            fs.renameSync(path, filename);
+            fileUrl = filename;
         }
-
-    })
-    // Post.create({
-
-    // })
-    res.json({title})
-
-}
+        try {
+            const post = await Post.create({
+                title,
+                summary,
+                content,
+                fileUrl 
+            });
+            res.json({ post });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Database error" });
+        }
+    });
+};
 
 module.exports = { post };  
